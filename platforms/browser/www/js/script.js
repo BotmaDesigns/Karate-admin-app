39534f92b80fc9d94ad0c7a79de6db4aaabfad08
BotmaDesigns <BotmaDesigns@users.noreply.github.com>
2021-02-11 13:39:52 +0200

    adding camera WIP

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -7,5 +7,33 @@ function myFunction() {
     }
 }
 
+function takePhoto(){
+	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+	destinationType: Camera.DestinationType.DATA_URL
+ }); 
 
+function onSuccess(imageData) {
+	var image = document.getElementById('myImage');
+	image.src = "data:image/jpeg;base64," + imageData;
+}
 
+function onFail(message) {
+	alert('Failed because: ' + message);
+}
+}
+
+function cameraTakePicture() { 
+   navigator.camera.getPicture(onSuccess, onFail, {  
+      quality: 50, 
+      destinationType: Camera.DestinationType.DATA_URL 
+   });  
+   
+   function onSuccess(imageData) { 
+      var image = document.getElementById('myImage'); 
+      image.src = "data:image/jpeg;base64," + imageData; 
+   }  
+   
+   function onFail(message) { 
+      alert('Failed because: ' + message); 
+   } 
+}
