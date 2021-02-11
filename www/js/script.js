fa47100416d5d0a7fcd646541029e7cd9123c70c
BotmaDesigns <BotmaDesigns@users.noreply.github.com>
2021-02-11 09:45:09 +0200

    karate admin app

---
+++ www/js/script.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+        x.className += " showMenu";
+    } else {
+        x.className = "menu";
+    }
+}
+
+function takePhoto(){
+	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.DATA_URL
+ }); 
+
+function onSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+}
