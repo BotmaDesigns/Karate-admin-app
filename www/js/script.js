b05a153edd5d5c940ae43ea1786f9bf28205c08f
BotmaDesigns <BotmaDesigns@users.noreply.github.com>
2021-02-11 11:15:12 +0200

    fixing camera

--- www/js/script.js
+++ www/js/script.js
@@ -7,17 +7,5 @@ function myFunction() {
     }
 }
 
-function takePhoto(){
-	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-    destinationType: Camera.DestinationType.DATA_URL
- }); 
 
-function onSuccess(imageData) {
-    var image = document.getElementById('myImage');
-    image.src = "data:image/jpeg;base64," + imageData;
-}
 
-function onFail(message) {
-    alert('Failed because: ' + message);
-}
-}
